1ce5dff462a6ad0a05dc95efd385f52af05856ff
bakigervalla <bakigervalla@gmail.com>
2021-10-15 09:50:14 +0200

    1 file modified, 1 added, compile-package electron app

--- du-editor/webpack.prod.config.js
+++ du-editor/webpack.prod.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
 const baseConfig = require('./webpack.config');
+const CopyWebpackPlugin = require('copy-webpackplugin');
+
 module.exports = {
   ...baseConfig,
   output: {
     path: path.join(process.cwd(), 'dist', 'myeditor'),
     filename: 'main.js',
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          context: path.join(process.cwd(), 'src', 'electron'),
+          from: 'package.json',
+        },
+      ],
+    }),
+  ],
 };
